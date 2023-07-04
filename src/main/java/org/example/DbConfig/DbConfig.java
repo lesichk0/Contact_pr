12e6e31ab596f2aceb8a1b779d68b3c7671e2f68
olesiaproskurnyak <olesiaproskurnyak@gmail.com>
2023-07-04 05:07:35 +0300

    added db configuration

---
+++ src/main/java/org/example/DbConfig/DbConfig.java
@@ -0,0 +1,10 @@
+package org.example.DbConfig;
+
+public class DbConfig {
+    public static final String contact_id = "id";
+    public static final String name_of_owner = "name";
+    public static final String email = "email";
+    public static final String phone_numb = "phone_number";
+    public static final String is_important = "is_important";
+    public static final String details = "details";
+}
